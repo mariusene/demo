2547b1ad1813d2e1133d2a1f43eac98b61b91c68
Marius Ene <mariusvlad.ene@gmail.com>
2018-05-10 01:18:22 +0300

    Add Authentication header on Swagger

---
+++ src/API/UCB.JapanCimzia.API/Controllers/AuthorizedOrdersController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace UCB.JapanCimzia.API.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/auth-orders")]
+    [Authorize]
+    public class AuthorizedOrdersController : OrdersController
+    {
+    }
+}
--- src/API/UCB.JapanCimzia.API/Startup/SwaggerConfig.cs
+++ src/API/UCB.JapanCimzia.API/Startup/SwaggerConfig.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
 using Swashbuckle.Application;
-
+using Swashbuckle.Swagger;
 
 namespace UCB.JapanCimzia.API
 {
@@ -11,11 +16,49 @@ namespace UCB.JapanCimzia.API
             config.EnableSwagger(c =>
             {
                 c.SingleApiVersion("v1", "UCB.JapanCimzia.API");
+                c.DescribeAllEnumsAsStrings();
+                c.OperationFilter<AuthorizationHeaderParameter>();
+                c.ApiKey("Authorization")
+                    .Description(
+                        "Standard Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"")
+                    .Name("Authorization")
+                    .In("header");
             })
             .EnableSwaggerUi(c =>
             {
                 //c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+        }
+
+        private class AuthorizationHeaderParameter : IOperationFilter
+        {
+            private readonly List<Type> _excludeTypes = new List<Type> { /*TODO: Controllers you want to exclude*/ };
+
+            public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+            {
+                var filterPipeline = apiDescription.ActionDescriptor.GetFilterPipeline();
+                var isAuthorized = filterPipeline
+                                                 .Select(filterInfo => filterInfo.Instance)
+                                                 .Any(filter => filter is IAuthorizationFilter);
+                var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+                var isExcluded = _excludeTypes.Contains(apiDescription.ActionDescriptor.ControllerDescriptor
+                    .ControllerType);
+
+                if (isExcluded || !isAuthorized || allowAnonymous)
+                    return;
+
+                if (operation.parameters == null)
+                    operation.parameters = new List<Parameter>();
+                operation.parameters.Add(new Parameter
+                {
+                    name = "Authorization",
+                    @default = "Bearer VALID_JWT_TOKEN_BASE64",
+                    description = "JWT Bearer Token",
+                    @in = "header",
+                    type = "string",
+                    required = true
+                });
+            }
         }
     }
 }
